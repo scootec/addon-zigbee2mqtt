d14877c66b171e393915b0e8c7e7cdca507f61d3
Shane Scott <shane.scott42@gmail.com>
2019-04-22 15:43:35 +1000

    removes quotes from file path

--- zigbee2mqtt/rootfs/etc/cont-init.d/zigbee2mqtt.sh
+++ zigbee2mqtt/rootfs/etc/cont-init.d/zigbee2mqtt.sh
@@ -7,12 +7,12 @@
 
 # Creates devices configuration files on first start.
 if ! bashio::fs.file_exists '/opt/zigbee2mqtt/data/devices.yaml'; then
-    touch '/opt/zigbee2mqtt/data/devices.yaml' \
+    touch /opt/zigbee2mqtt/data/devices.yaml \
         || bashio::exit.nok "Could not create devices.yaml."
 fi
 
 # Creates groups configuration files on first start.
 if ! bashio::fs.file_exists '/opt/zigbee2mqtt/data/groups.yaml'; then
-    touch '/opt/zigbee2mqtt/data/groups.yaml' \
+    touch /opt/zigbee2mqtt/data/groups.yaml \
         || bashio::exit.nok "Could not create groups.yaml."
 fi
