8d2173f66f8293319e2d7cafe1c5678c480dd269
Shane Scott <shane.scott42@gmail.com>
2019-04-22 16:17:50 +1000

    more loggins

--- zigbee2mqtt/rootfs/etc/cont-init.d/zigbee2mqtt.sh
+++ zigbee2mqtt/rootfs/etc/cont-init.d/zigbee2mqtt.sh
@@ -6,19 +6,25 @@
 
 # Symlinks configuration directory on first start.
 if ! bashio::fs.file_exists '/opt/zigbee2mqtt/data/configuration.yaml'; then
-    rm -rf /opt/zigbee2mqtt/data && \
+    bashio::log "Deleting /opt/zigbee2mqtt/data folder."
+    rm -rf /opt/zigbee2mqtt/data \
+    || bashio::exit.nok "Could not delete /opt/zigbee2mqtt/data folder."
+
+    bashio::log "Symlinking /opt/zigbee2mqtt/data to persistant storage."
     ln -s /data /opt/zigbee2mqtt/data \
-        || bashio::exit.nok "Could not symlink configuration.yaml."
+    || bashio::exit.nok "Could not symlink /opt/zigbee2mqtt/data to persistant storage."
 fi
 
 # Creates devices configuration files on first start.
 if ! bashio::fs.file_exists '/opt/zigbee2mqtt/data/devices.yaml'; then
+    bashio::log "Creating devices.yaml."
     touch /opt/zigbee2mqtt/data/devices.yaml \
         || bashio::exit.nok "Could not create devices.yaml."
 fi
 
 # Creates groups configuration files on first start.
 if ! bashio::fs.file_exists '/opt/zigbee2mqtt/data/groups.yaml'; then
+    bashio::log "Creating groups.yaml."
     touch /opt/zigbee2mqtt/data/groups.yaml \
         || bashio::exit.nok "Could not create groups.yaml."
 fi
