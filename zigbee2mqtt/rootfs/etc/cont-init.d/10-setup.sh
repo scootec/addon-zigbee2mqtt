130d0a657692044258f0d8c710fe0d35661251d9
Shane Scott <shane.scott42@gmail.com>
2019-12-14 10:33:15 +1100

    more changes!

---
+++ zigbee2mqtt/rootfs/etc/cont-init.d/10-setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Zigbee2mqtt
+# Sets up Zigbee2mqtt.
+# ==============================================================================
+
+# Creates configuration folder if it does not exist.
+if ! bashio::fs.directory_exists '/config/zigbee2mqtt'; then
+    bashio::log "Creating zigbee2mqtt folder in /config."
+    mkdir /config/zigbee2mqtt \
+        || bashio::exit.nok "Could not create /config/zigbee2mqtt."
+fi
+
+# Creates devices configuration files on first start.
+if ! bashio::fs.file_exists '/config/zigbee2mqtt/devices.yaml'; then
+    bashio::log "Creating devices.yaml."
+    touch /config/zigbee2mqtt/devices.yaml \
+        || bashio::exit.nok "Could not create devices.yaml."
+fi
+
+# Creates groups configuration files on first start.
+if ! bashio::fs.file_exists '/config/zigbee2mqtt/groups.yaml'; then
+    bashio::log "Creating groups.yaml."
+    touch /config/zigbee2mqtt/groups.yaml \
+        || bashio::exit.nok "Could not create groups.yaml."
+fi
+
+# Links /opt/zigbee2mqtt/data to /config/zigbee2mqtt
+if ! bashio::fs.directory_exists '/opt/zigbee2mqtt/data'; then
+    bashio::log "Linking /opt/zigbee2mqtt/data to /config/zigbee2mqtt."
+    ln -s /config/zigbee2mqtt /opt/zigbee2mqtt/data \
+        || bashio::exit.nok "Could not create symbolic link."
+fi
